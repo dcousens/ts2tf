3f201a78a53d0745db8c855868574333ca170626
Daniel Cousens <dcousens@users.noreply.github.com>
2018-12-06 15:40:35 +1100

    support basic types

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # typescript-2-typeforce
-A bikeshed tool that maps typescript types to typeforce code 
+A bikeshed tool that maps typescript types to typeforce code
+
+### Example
+``` bash
+node index.js test.d.ts
+```
--- index.js
+++ index.js
@@ -7,17 +7,56 @@ const TOKEN = ts.SyntaxKind
 const code = fs.readFileSync(process.argv[2]).toString('utf8')
 const root = ts.createSourceFile('tmp.ts', code, ts.ScriptTarget.Latest, true)
 
+function getType (type) {
+  switch (type.kind) {
+    case TOKEN.BooleanKeyword: return 'boolean'
+    case TOKEN.NumberKeyword: return 'number'
+    case TOKEN.StringKeyword: return 'string'
+    case TOKEN.LiteralType: return { literal: type.getText() }
+    case TOKEN.TypeLiteral: return type.members.map(getNode)
+    case TOKEN.ArrayType: {
+      return { array: getType(type.elementType) }
+    }
+    case TOKEN.TypeReference: {
+      return { ref: type.getText() }
+    }
+    case TOKEN.IntersectionType: {
+      return { and: type.types.map(getType) }
+    }
+    case TOKEN.UnionType: {
+      return { or: type.types.map(getType) }
+    }
+    case TOKEN.IndexedAccessType: {
+      return {
+        iref: getType(type.objectType).ref,
+        ikey: getType(type.indexType).literal
+      }
+    }
+  }
+
+  return undefined
+}
+
 function getNode (node) {
   switch (node.kind) {
     case TOKEN.EndOfFileToken: return
 
     case TOKEN.InterfaceDeclaration: {
-      return {}
+      const name = node.name.getText()
+      return {
+        name,
+        type: node.members.map(m => getNode(m))
+      }
     }
 
     case TOKEN.TypeAliasDeclaration:
     case TOKEN.PropertySignature: {
-      return {}
+      const name = node.name.getText()
+      const type = getType(node.type)
+      const maybe = !!node.questionToken
+      const result = { name, type }
+      if (maybe) result.maybe = true
+      return result
     }
   }
 }
